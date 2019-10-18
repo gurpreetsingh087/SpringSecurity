9b769075ccc80e808db84e9a64fd90ec01d70eb4
Singh Gurpreet <10054059@gemalto.com>
2019-10-18 17:30:47 +0530

    Form-Login with Spring-Security-Configuration

---
+++ FormLogin/src/main/java/com/spring/security/config/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/login").permitAll()
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/**").hasAnyRole("ADMIN", "USER")
+        .and().formLogin()
+        .and().logout().logoutSuccessUrl("/login").permitAll()
+        .and().csrf().disable();
+    }*/
+    
+    
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//auth.inMemoryAuthentication().withUser("user").password("password"); /* WILL NOT WORK*/		
+		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("admin");
+	}
+}
