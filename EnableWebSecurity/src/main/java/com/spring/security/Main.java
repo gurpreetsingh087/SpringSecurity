98692a6b2b0245fa317ebb1eba9c14dbf683c1e6
Singh Gurpreet <10054059@gemalto.com>
2019-10-27 11:27:44 +0530

    Code Updated

--- EnableWebSecurity/src/main/java/com/spring/security/Main.java
+++ EnableWebSecurity/src/main/java/com/spring/security/Main.java
@@ -3,6 +3,7 @@ package com.spring.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class }) //Disable defualt security
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {		
